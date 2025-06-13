a2bb2c8585ca26e1de5a206a1a25e0e471d46eb3
Tyler Peryea <tylerperyea@gmail.com>
2025-06-13 07:32:23 -0400

    Update inchi-raw.js

--- JSChemify/inchi/inchi-raw.js
+++ JSChemify/inchi/inchi-raw.js
@@ -152,7 +152,7 @@ if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_WORKER) {
   } else {
     scriptDirectory = scriptDirectory.slice(0, scriptDirectory.replace(/[?#].*/, '').lastIndexOf('/')+1);
   }
-
+  scriptDirectory = "https://tylerperyea.github.io/JSChemify/inchi/";
   if (!(typeof window == 'object' || typeof WorkerGlobalScope != 'undefined')) throw new Error('not compiled for this environment (did you build to HTML and try to run it not on the web, or set ENVIRONMENT to something - like node - and run it someplace else - like on the web?)');
 
   {
@@ -4384,3 +4384,100 @@ if (typeof exports === 'object' && typeof module === 'object') {
   module.exports.default = inchiModule107;
 } else if (typeof define === 'function' && define['amd'])
   define([], () => inchiModule107);
+
+"use strict";
+
+/*
+ * WASM module(s) initialization
+ *
+ * Calling the factory function returns a Promise which resolves to the module object.
+ * See https://github.com/emscripten-core/emscripten/blob/fa339b76424ca9fbe5cf15faea0295d2ac8d58cc/src/settings.js#L1183
+ */
+const availableInchiVersions = {
+  "1.07.3": {
+    module: inchiModule107(),
+    optionsTemplateId: "inchiOptionsTemplate107",
+    default: true,
+  }
+};
+
+/*
+ * Glue code to invoke the C functions in inchi_web.c
+ *
+ * Char pointers returned by inchi_from_molfile, inchikey_from_inchi,
+ * molfile_from_inchi and molfile_from_auxinfo need to be freed here.
+ * See https://github.com/emscripten-core/emscripten/issues/6484 (Emscripten does
+ * not do this on its own when using "string" as return type.)
+ */
+async function inchiFromMolfile(molfile, options, inchiVersion) {
+  const module = await availableInchiVersions[inchiVersion].module;
+  const ptr = module.ccall(
+    "inchi_from_molfile",
+    "number",
+    ["string", "string"],
+    [molfile, options]
+  );
+  const result = module.UTF8ToString(ptr);
+  module._free(ptr);
+
+  return JSON.parse(result);
+}
+
+async function inchikeyFromInchi(inchi, inchiVersion) {
+  const module = await availableInchiVersions[inchiVersion].module;
+  const ptr = module.ccall(
+    "inchikey_from_inchi",
+    "number",
+    ["string"],
+    [inchi]
+  );
+  const result = module.UTF8ToString(ptr);
+  module._free(ptr);
+
+  return JSON.parse(result);
+}
+
+async function molfileFromInchi(inchi, options, inchiVersion) {
+  const module = await availableInchiVersions[inchiVersion].module;
+  const ptr = module.ccall(
+    "molfile_from_inchi",
+    "number",
+    ["string", "string"],
+    [inchi, options]
+  );
+  const result = module.UTF8ToString(ptr);
+  module._free(ptr);
+
+  return JSON.parse(result);
+}
+
+async function molfileFromAuxinfo(
+  auxinfo,
+  bDoNotAddH,
+  bDiffUnkUndfStereo,
+  inchiVersion
+) {
+  const module = await availableInchiVersions[inchiVersion].module;
+  const ptr = module.ccall(
+    "molfile_from_auxinfo",
+    "number",
+    ["string", "number", "number"],
+    [auxinfo, bDoNotAddH, bDiffUnkUndfStereo]
+  );
+  const result = module.UTF8ToString(ptr);
+  module._free(ptr);
+
+  return JSON.parse(result);
+}
+
+if (typeof module === "object" && module.exports) {
+  // Only export functions in Node. See https://github.com/umdjs/umd.
+  // Prevents "Uncaught ReferenceError: module is not defined" in browser.
+  module.exports = {
+    inchiFromMolfile,
+    inchikeyFromInchi,
+    molfileFromInchi,
+    molfileFromAuxinfo,
+    availableInchiVersions,
+  };
+}
